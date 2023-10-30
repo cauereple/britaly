ec1a27b87863f4812c20276e5d8c311244b17cc5
Cauê Reple <caue-reple@hotmail.com>
2023-10-30 18:52:17 +0100

    Creating customer address class

---
+++ application/customer/src/main/java/com/britaly/customer/adapter/in/api/request/Address.java
@@ -0,0 +1,11 @@
+package com.britaly.customer.adapter.in.api.request;
+
+public class Address {
+
+    private String address;
+    private int number;
+    private String complement;
+    private String city;
+    private String State;
+    private Country country;
+}
