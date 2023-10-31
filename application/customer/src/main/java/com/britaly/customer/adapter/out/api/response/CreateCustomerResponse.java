ee3efc5673bc8ce5a267084d44b3ca6083d90d51
Cauê Reple <caue-reple@hotmail.com>
2023-10-31 23:00:44 +0100

    Creating CreateCustomerResponse class

---
+++ application/customer/src/main/java/com/britaly/customer/adapter/out/api/response/CreateCustomerResponse.java
@@ -0,0 +1,10 @@
+package com.britaly.customer.adapter.out.api.response;
+
+import lombok.Data;
+
+@Data
+public class CreateCustomerResponse {
+    
+    private Integer id;
+    private String uuid;
+}
